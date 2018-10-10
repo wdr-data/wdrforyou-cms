e140a396da82c4c2f00f81276c0b4bb0dd39eaa5
LizzAy <lisachenbach@gmx.de>
2018-10-10 10:37:16 +0200

    :sparkles: Add sending published reports automatic

--- app/cms/admin/report.py
+++ app/cms/admin/report.py
@@ -1,11 +1,19 @@
-from django.contrib import admin
+import os
+from time import sleep
+from posixpath import join as urljoin
+
+from django.contrib import admin, messages
+from django.db import transaction
 from django.utils import timezone
 from django import forms
 from emoji_picker.widgets import EmojiPickerTextInput
 from emoji_picker.widgets import EmojiPickerTextarea
+import requests
 
 from ..models.report import Report, ReportTranslation
 from .translation import TranslationModelForm, TranslationAdminInline
+
+PUSH_TRIGGER_URL = urljoin(os.environ['BOT_SERVICE_ENDPOINT'], 'sendReport')
 
 class ReportTranslationModelForm(TranslationModelForm):
 
@@ -39,8 +47,31 @@ class ReportModelForm(forms.ModelForm):
 
 class ReportAdmin(admin.ModelAdmin):
     form = ReportModelForm
-    
     inlines = (ReportTranslationAdminInline, )
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+    
+        try:
+            if obj.published and not obj.delivered:
+
+                def commit_hook():
+                    sleep(1)  # Wait for DB
+                    r = requests.post(
+                        url=PUSH_TRIGGER_URL,
+                        json={'id': obj.id}
+                    )
+
+                    if r.status_code == 200:
+                        messages.success(request, '🚨 Nachricht wird jetzt gesendet...')
+
+                    else:
+                        messages.error(request, '🚨 Nachricht konnte nicht gesendet werden!')
+
+                transaction.on_commit(commit_hook)
+
+        except Exception as e:
+            messages.error(request, str(e))
 
 
 # Register your models here.

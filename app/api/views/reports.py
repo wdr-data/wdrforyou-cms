2decb06cba7b6fc22c5587650a32c4c798fed261
ChristianJoe <cjoe@gmx.de>
2019-05-08 10:31:27 +0200

    :sparkles: Allow filtering reports by published translations

--- app/api/views/reports.py
+++ app/api/views/reports.py
@@ -15,6 +15,19 @@ class ReportSerializer(ModelSerializerWithTranslations):
 
 
 class ReportViewSet(viewsets.ModelViewSet):
-    queryset = Report.objects.filter(published=True).order_by('-created')
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Report.objects.filter(published=True).order_by('-created')
+        language = self.request.query_params.get('language', None)
+
+        if language is not None:
+            queryset = queryset.filter(translation__language=language, translation__published=True)
+
+        return queryset
+
     serializer_class = ReportSerializer
     filter_fields = ('arabic', 'persian', 'english', 'published', 'delivered')
